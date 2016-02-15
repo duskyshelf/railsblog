444284c31addbc8636aa6817c376bfaf518c51d9
DuskyShelf <dupsdale@gmail.com>
2016-02-15 19:30:16 +0000

    added xml api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,12 @@ class PostsController < ApplicationController
   end
 
   def api
-    posts = Post.all
-    render(json: posts.to_json)
+    @posts = Post.all
+
+    respond_to do |format|
+      format.xml { render :xml => @posts }
+      format.json { render :json => @posts }
+    end
   end
 
   private
